57dc00bc9c4bcc35d6e90513fcf59b94d9d93f79
Andres David Restrepo Hernandez <adrestreph@eafit.edu.co>
2025-11-11 06:21:53 -0500

    update taller2

---
+++ Diseño de Algoritmos/Diseño de Algoritmos 2/Taller 2/Ejercicio1.py
@@ -0,0 +1,5 @@
+"""
+Crea un programa donde implementes una funcion para calcular las principales operaciones aritmeticas de dos numeros, la
+funcion tendra 3 parametros (operacion, num1,num2) y debe devolver
+"""
+import os
